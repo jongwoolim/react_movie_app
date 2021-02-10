44866f9e00114609f154351d9a3c5a34b741fd4d
limjongwoo <whddn528@naver.com>
2021-02-10 16:03:16 +0900

    feat: navigation add

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {HashRouter, Route} from 'react-router-dom';
 import './App.css';
 import About from './routes/About';
 import Home from './routes/Home';
+import Navigation from './components/Navigation';
 
 function App() {
   
   return (
     <HashRouter>
+      <Navigation/>
       <Route path="/" exact = {true} component={Home}/>
       <Route path="/about" component={About}/>
     </HashRouter>
